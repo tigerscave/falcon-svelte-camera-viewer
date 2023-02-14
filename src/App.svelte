c2a75dafdf1884b58fc61a0fd340447feae491ce
Falcon <falcon@adawarp.com>
2023-02-14 18:23:12 +0900

    review fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,28 @@
 <script>
   import VideoStream from './components/VideoStream.svelte'
-  let ipAddressA = localStorage.getItem('firstIPaddress')
-  let ipAddressB = localStorage.getItem('secondIPaddress')
-  let ping_view = ''
-
   import { io } from "socket.io-client";
   const socket = io("http://127.0.0.1:5001");
+  let ipAddressA = localStorage.getItem('firstIPaddress')
+  let ipAddressB = localStorage.getItem('secondIPaddress')
+  let pingResultText = "Not Connected"
 
-  // client-side
-  socket.on("connect", () => {
-    console.log("---connected---")
-    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-  });
-
-  //ping-check
-  function ping_insert(ping_value){
-    if(ping_value){
-      return ping_view = ping_value
-    }
-    else{
-      return ping_view = '0'
-    }
-    }
-
-  function trigger(){
-    socket.emit('trigger');
+  function pingCheck(){
+    socket.emit('ask_ping');
   }
   
   socket.on('connect', function() { 
-    socket.on('receive_ping', (msg) => { 
-      ping_insert(msg)
+    socket.on('receive_ping', (value) => { 
+      pingResultText = value ? 'Online' : 'Offline'
+      console.log(pingResultText)
     });
   });
 
-  setInterval(trigger,'1000')
+  setInterval(pingCheck,1000)
 </script>
 
 <main>
   <h1>Svelte Camera Viewer</h1>
-  <p>{ping_view}</p>
+  <p>{pingResultText}</p>
 <VideoStream ipAddress={ipAddressA} stragekey='firstIPaddress'/>
 <VideoStream ipAddress={ipAddressB} stragekey='secondIPaddress'/>
 
